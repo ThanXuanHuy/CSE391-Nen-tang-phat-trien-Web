c507f854f2282bb67a663cdc4a017fdd9efb0b1d
Xuân Huy <166245579+ThanXuanHuy@users.noreply.github.com>
2025-07-31 15:46:52 +0700

    Add files via upload

---
+++ Đề 2/De2.js
@@ -0,0 +1,37 @@
+const transactions = [
+  {
+    id: 1102,
+    khachhang: "Võ Hoài An",
+    nhanvien: "Mai Thục Anh",
+    sotien: 250000,
+    ngaymua: "2024-06-06T09:00:00"
+  },
+  {
+    id: 1199,
+    khachhang: "Hoàng Thị Thắng",
+    nhanvien: "Nguyễn Văn Hồng",
+    sotien: 600000,
+    ngaymua: "2024-06-06T09:03:00"
+  },
+  {
+    id: 1239,
+    khachhang: "Nguyễn Huy Quang",
+    nhanvien: "Nguyễn Văn Hồng",
+    sotien: 934000,
+    ngaymua: "2024-06-06T09:10:00"
+  },
+  {
+    id: 1677,
+    khachhang: "Huỳnh Văn Nam",
+    nhanvien: "Mai Thục Anh",
+    sotien: 150000,
+    ngaymua: "2024-06-06T09:20:00"
+  },
+  {
+    id: 2100,
+    khachhang: "Nguyễn Minh Minh",
+    nhanvien: "Mai Thục Anh",
+    sotien: 354000,
+    ngaymua: "2024-06-06T09:24:00"
+  }
+];
